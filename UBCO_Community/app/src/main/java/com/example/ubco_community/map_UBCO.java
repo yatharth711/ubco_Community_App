d464779f2f86f890ecad4ce680f795121b1636bd
Yatharth Mathur <67724181+yatharth711@users.noreply.github.com>
2022-12-05 19:25:34 -0800

    Added Files for front end

---
+++ UBCO_Community/app/src/main/java/com/example/ubco_community/map_UBCO.java
@@ -0,0 +1,14 @@
+package com.example.ubco_community;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class map_UBCO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_ubco);
+    }
+}
